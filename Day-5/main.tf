5d42fd5e9b6f2918fd8d8170d4df1eb7d5440179
Manideep4427 <Manideepreddy4428@gmail.com>
2026-06-03 15:14:43 +0530

    Enhance comments in main.tf for better understanding
    Added comments for clarity in the connection block and command execution.

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "server" {
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
 
-  connection {
+  connection {          # We are connecting to the instance 
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
     private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
@@ -99,7 +99,7 @@ resource "aws_instance" "server" {
       "sudo apt-get install -y python3-pip",  # Example package installation
       "cd /home/ubuntu",
       "sudo pip3 install flask",
-      "sudo python3 app.py &",
+      "sudo python3 app.py &",           # Running the python app ,If not running remmove the &
     ]
   }
 }
