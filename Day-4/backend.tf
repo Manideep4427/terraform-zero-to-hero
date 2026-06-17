9016b18b18d370051ac5269e3c02ae15e9663251
Manideep4427 <Manideepreddy4428@gmail.com>
2026-06-17 13:30:42 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    key            = "abhi/terraform.tfstate" # just the path to s3 bucket
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
